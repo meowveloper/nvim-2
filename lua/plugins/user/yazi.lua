d1319edfa23f5735ea1df0cd1b8744a798dbb436
meowveloper <meowveloper.dev@gmail.com>
2025-11-12 13:35:55 +0700

    yazi

---
+++ lua/plugins/user/yazi.lua
@@ -0,0 +1,41 @@
+return {
+    ---@type LazySpec
+    {
+        "mikavilpas/yazi.nvim",
+        version = "*", -- use the latest stable version
+        event = "VeryLazy",
+        dependencies = {
+            { "nvim-lua/plenary.nvim", lazy = true },
+        },
+        keys = {
+            -- 👇 in this section, choose your own keymappings!
+            {
+                "<leader>ee",
+                mode = { "n", "v" },
+                "<cmd>Yazi<cr>",
+                desc = "Open yazi at the current file",
+            },
+            {
+                -- Open in the current working directory
+                "<leader>ew",
+                "<cmd>Yazi cwd<cr>",
+                desc = "Open the file manager in nvim's working directory",
+            },
+            {
+                "<leader>eq",
+                "<cmd>Yazi toggle<cr>",
+                desc = "Resume the last yazi session",
+            },
+        },
+        ---@type YaziConfig | {}
+        opts = {
+            open_for_directories = false,
+            keymaps = {
+                show_help = "<f1>",
+            },
+        },
+        init = function()
+            vim.g.loaded_netrwPlugin = 1
+        end,
+    },
+}
